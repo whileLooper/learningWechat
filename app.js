779ce18c1aceff35aa2a730c58bd6a1eab16610f
b004577 <bo.chen@macys.com>
2018-01-05 16:36:02 -0500

    rebuild with koa1

--- app.js
+++ app.js
@@ -1,95 +1,40 @@
-const Koa = require('koa')
-const app = new Koa()
-const views = require('koa-views')
-const json = require('koa-json')
-const onerror = require('koa-onerror')
-const bodyparser = require('koa-bodyparser')
-const logger = require('koa-logger')
+var app = require('koa')()
+  , logger = require('koa-logger')
+  , json = require('koa-json')
+  , views = require('koa-views')
+  , onerror = require('koa-onerror');
 
-const index = require('./routes/index')
-const users = require('./routes/users')
-const wechat = require('co-wechat');
-const wechatConfig = {
-  port: '80',
-  token: 'bhu89ijnmko0',
-  appid: 'wxd61026a80b610e7c',
-  appSecret: '0e49baa28dd88439372c4788a619aa16',
-  encodingAESKey: 'HX2DzbFRh9qY4toNv9XVL1kYxgoYCTwoZ3haN92kAzl',
-  apiDomain: 'https://api.weixin.qq.com/',
-  accessTokenFilePath: './static/access_token.json'
-};
+var index = require('./routes/index');
+var users = require('./routes/users');
 
 // error handler
-onerror(app)
+onerror(app);
 
-// middlewares
-app.use(bodyparser({
-  enableTypes:['json', 'form', 'text']
-}))
-app.use(json())
-app.use(logger())
-app.use(require('koa-static')(__dirname + '/public'))
-
-app.use(views(__dirname + '/views', {
-  extension: 'pug'
-}))
-
-// logger
-app.use(async (ctx, next) => {
-  const start = new Date()
-  await next()
-  const ms = new Date() - start
-  console.log(`${ctx.method} ${ctx.url} - ${ms}ms`)
-})
-
-app.use(wechat(wechatConfig).middleware(async (message, ctx) => {
-  // 微信输入信息就是这个 message
-  if (message.FromUserName === 'diaosi') {
-    // 回复屌丝(普通回复)
-    return 'hehe';
-  } else if (message.FromUserName === 'text') {
-    //你也可以这样回复text类型的信息
-    return {
-      content: 'text object',
-      type: 'text'
-    };
-  } else if (message.FromUserName === 'hehe') {
-    // 回复一段音乐
-    return {
-      type: "music",
-      content: {
-        title: "来段音乐吧",
-        description: "一无所有",
-        musicUrl: "http://mp3.com/xx.mp3",
-        hqMusicUrl: "http://mp3.com/xx.mp3"
-      }
-    };
-  } else if (message.FromUserName === 'kf') {
-    // 转发到客服接口
-    return {
-      type: "customerService",
-      kfAccount: "test1@test"
-    };
-  } else {
-    // 回复高富帅(图文回复)
-    return [
-      {
-        title: '你来我家接我吧',
-        description: '这是女神与高富帅之间的对话',
-        picurl: 'http://nodeapi.cloudfoundry.com/qrcode.jpg',
-        url: 'http://nodeapi.cloudfoundry.com/'
-      }
-    ];
-  }
+// global middlewares
+app.use(views('views', {
+  root: __dirname + '/views',
+  default: 'jade'
 }));
+app.use(require('koa-bodyparser')());
+app.use(json());
+app.use(logger());
 
-// routes
-app.use(index.routes(), index.allowedMethods())
-app.use(users.routes(), users.allowedMethods())
+app.use(function *(next){
+  var start = new Date;
+  yield next;
+  var ms = new Date - start;
+  console.log('%s %s - %s', this.method, this.url, ms);
+});
+
+app.use(require('koa-static')(__dirname + '/public'));
+
+// routes definition
+app.use(index.routes(), index.allowedMethods());
+app.use(users.routes(), users.allowedMethods());
 
 // error-handling
 app.on('error', (err, ctx) => {
   console.error('server error', err, ctx)
 });
 
-module.exports = app
+module.exports = app;
