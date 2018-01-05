375b985f408b186ddcb0d8292974f42c5cfafd93
b004577 <bo.chen@macys.com>
2018-01-05 13:41:59 -0500

    project init

--- app.js
+++ app.js
@@ -8,6 +8,16 @@ const logger = require('koa-logger')
 
 const index = require('./routes/index')
 const users = require('./routes/users')
+const wechat = require('co-wechat');
+const wechatConfig = {
+  port: '80',
+  token: 'bhu89ijnmko0',
+  appid: 'wxd61026a80b610e7c',
+  appSecret: '0e49baa28dd88439372c4788a619aa16',
+  encodingAESKey: 'HX2DzbFRh9qY4toNv9XVL1kYxgoYCTwoZ3haN92kAzl',
+  apiDomain: 'https://api.weixin.qq.com/',
+  accessTokenFilePath: './static/access_token.json'
+};
 
 // error handler
 onerror(app)
@@ -31,6 +41,47 @@ app.use(async (ctx, next) => {
   const ms = new Date() - start
   console.log(`${ctx.method} ${ctx.url} - ${ms}ms`)
 })
+
+app.use(wechat(wechatConfig).middleware(async (message, ctx) => {
+  // 微信输入信息就是这个 message
+  if (message.FromUserName === 'diaosi') {
+    // 回复屌丝(普通回复)
+    return 'hehe';
+  } else if (message.FromUserName === 'text') {
+    //你也可以这样回复text类型的信息
+    return {
+      content: 'text object',
+      type: 'text'
+    };
+  } else if (message.FromUserName === 'hehe') {
+    // 回复一段音乐
+    return {
+      type: "music",
+      content: {
+        title: "来段音乐吧",
+        description: "一无所有",
+        musicUrl: "http://mp3.com/xx.mp3",
+        hqMusicUrl: "http://mp3.com/xx.mp3"
+      }
+    };
+  } else if (message.FromUserName === 'kf') {
+    // 转发到客服接口
+    return {
+      type: "customerService",
+      kfAccount: "test1@test"
+    };
+  } else {
+    // 回复高富帅(图文回复)
+    return [
+      {
+        title: '你来我家接我吧',
+        description: '这是女神与高富帅之间的对话',
+        picurl: 'http://nodeapi.cloudfoundry.com/qrcode.jpg',
+        url: 'http://nodeapi.cloudfoundry.com/'
+      }
+    ];
+  }
+}));
 
 // routes
 app.use(index.routes(), index.allowedMethods())
