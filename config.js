e02b7b77a30c7508746839a641f0e6812168eea7
b004577 <bo.chen@macys.com>
2018-01-21 23:48:46 -0500

    change to real app config

--- config.js
+++ config.js
@@ -6,9 +6,9 @@ var wechat_file = path.join(__dirname, './config/wechat.txt');
 // wechat
 var config = {
   wechat: {
-    token: 'mywechatdemo',
-    appID: 'wx58080b1fe9cb4f49',
-    appSecret: '9dba811200ddd73da9a24d7f36762485',
+    token: 'bhu89ijnmko0',
+    appID: 'wxd61026a80b610e7c',
+    appSecret: '7b9dde29e00e60d9474551e06e409c3f587a45fca820916e183acca9e41839e',
     getAccessToken: function() {
       return util.readFileAsync(wechat_file);
     },
